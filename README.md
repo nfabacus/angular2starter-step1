87ef326c3c5c5bf055f66c4281404dbc51c26d99
nobuyuki.fujioka <nobuyuki.fujioka@ecotricity.co.uk>
2017-02-27 21:44:42 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Angular 2 Starter
+
+  First, install angular CLI
+  
+  This includes .atomignore file.
+  It shows basic component setup.
---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bs-app',
+  template: `
+    <h1> {{pageTitle}} </h1>
+    <div>App Component Test</div>
+    <bs-welcome></bs-welcome>
+  `
+})
+
+export class AppComponent {
+  pageTitle: string = "Dev Book Store!"
+}
